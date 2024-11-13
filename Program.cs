9bebfd0acb5d83e82bd8a057328a030ea4fc7999
abraao0liveira <abraaooliveira.br@gmail.com>
2024-11-12 22:04:07 -0300

    update progressive and regressive

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using System.ComponentModel.Design;
-using System.Data;
-using System.Runtime.CompilerServices;
-
-namespace StopWatch
+﻿namespace StopWatch
 {
   class Program
   {
@@ -15,30 +11,62 @@ namespace StopWatch
     {
       Console.Clear();
 
-      Console.WriteLine("S = Seconds => 10s = 10 seconds\nM = Minutes => 1m = 1 minute\n0 = Exist");
+      Console.WriteLine("S = Seconds => 10s = 10 seconds\nM = Minutes => 1m = 1 minute\nH = Hours => 1h = 1 hour\n0 = Exist");
       Console.WriteLine("How long do you want to count?");
 
       string data = (Console.ReadLine() ?? "").ToLower();
+
+      if (data == "0")
+      {
+        Console.Clear();
+        Environment.Exit(0);
+      }
+
       char type = char.Parse(data.Substring(data.Length - 1, 1)); // Pega o último caractere da string
       int time = int.Parse(data.Substring(0, data.Length - 1)); // Pega todos os caracteres menos o último
 
-      int multiplier = 1;
+      Console.WriteLine("Tempo progressivo ou regressivo? (P/R)");
+      char progress = char.Parse((Console.ReadLine() ?? "").ToLower());
 
-      if (type == 'm')
+      if (progress == 'p')
       {
-        multiplier = 60;
+        int multiplier = 1;
+        if (type == 'm')
+        {
+          multiplier = 60;
+        }
+        else if (type == 'h')
+        {
+          multiplier = 3600;
+        }
+        else
+        {
+          PreStartProgressive(time * multiplier);
+        }
       }
-      else if (time == 0)
+      else if (progress == 'r')
       {
-        Environment.Exit(0);
+        int multiplier = 1;
+        if (type == 'm')
+        {
+          multiplier = 60;
+        }
+        else if (type == 'h')
+        {
+          multiplier = 3600;
+        }
+        else
+        {
+          PreStartRegressive(time * multiplier);
+        }
       }
       else
       {
-        PreStart(time * multiplier);
+        Menu();
       }
     }
 
-    static void PreStart(int time)
+    static void PreStartProgressive(int time)
     {
       Console.Clear();
       Console.WriteLine("Ready...");
@@ -48,10 +76,23 @@ namespace StopWatch
       Console.WriteLine("Go!");
       Thread.Sleep(2500);
 
-      Start(time);
+      StartProgressive(time);
     }
 
-    static void Start(int time)
+    static void PreStartRegressive(int time)
+    {
+      Console.Clear();
+      Console.WriteLine("Ready...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Set...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Go!");
+      Thread.Sleep(2500);
+
+      StartRegressive(time);
+    }
+
+    static void StartProgressive(int time)
     {
       int currentTime = 0;
 
@@ -62,6 +103,22 @@ namespace StopWatch
         currentTime++;
         Console.WriteLine(currentTime);
         Thread.Sleep(1000);
+      }
+
+      Console.Clear();
+      Console.WriteLine("Time's up!");
+      Thread.Sleep(1000);
+      Menu();
+    }
+
+    static void StartRegressive(int time)
+    {
+      while (time != 0)
+      {
+        Console.Clear();
+        Console.WriteLine(time);
+        Thread.Sleep(1000);
+        time--;
       }
 
       Console.Clear();
